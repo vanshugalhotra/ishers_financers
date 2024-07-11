359fdddd13a1ae98fb5d127815dd183cddc5f804
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2024-07-11 12:58:33 +0530

    design: added background image

--- app/page.js
+++ app/page.js
@@ -1,8 +1,21 @@
+"use client";
+import { useSidebar } from "@/context/SidebarContext";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="w-full">
+  const { marginForSidebar } = useSidebar();
 
+  return (
+    <main className="overflow-hidden" style={{ marginLeft: marginForSidebar }}>
+      <Image
+        alt="Finance"
+        className="w-[95vw] h-[94vh]"
+        layout="fixed"
+        width={1000}
+        height={1000}
+        objectFit="cover"
+        src={`/assets/images/background/bg.png`}
+      />
     </main>
   );
 }
